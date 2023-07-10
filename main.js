672d2899698916213eefa8ba0da855ac56bb54d6
Nancy D. Arena <ndarena11@gmail.com>
2023-07-09 20:11:54 -0400

    complete lab

--- main.js
+++ main.js
@@ -3,9 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hide = document.getElementById('modal');
+hide.className = 'hidden';
 
-
-
+const likes = document.querySelectorAll('.like-glyph');
+console.log(likes)
+for (const like of likes) {
+  like.addEventListener('click', fillheart)
+}
+function fillheart (event) {
+  console.log(event.target);
+  mimicServerCall('url')
+  .then(function() {
+    if (event.target.innerText === EMPTY_HEART) {
+      event.target.innerText = FULL_HEART
+      event.target.className = 'activated-heart'
+    } else {
+      event.target.innerText = EMPTY_HEART
+      event.target.className = ''
+    }
+  })
+  .catch(function(error){
+    hide.className = ''
+    hide.innerText = error
+    setTimeout(() => {
+      hide.classList.add('hidden')
+    }, 3000);
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
